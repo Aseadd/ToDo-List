4138915779c018e7ff23241896e5e477911fb4ed
addis <betsega23@gmail.com>
2022-08-10 23:22:22 +0300

    Add style and Array for todo list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('body');
+const taskInput = document.querySelector('.task-input input');
+let todos = JSON.parse(localStorage.getItem('todo-list'));
+const taskBox = document.querySelector('.task-box');
 
-  // Lodash, currently included via a script, is required for this line to work
-  //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.style.background = ' green';
-  return element;
-}
+const showTodo = () => {
+  let li = '';
 
-document.body.appendChild(component());
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let isCompleted = todo.status == 'completed' ? 'checked' : '';
+      li += ` <li class="task">
+              <label for="${id}">
+                <input class='task-item' type="checkbox" id="${id}" ${isCompleted}>
+                <p class='${isCompleted}'> ${todo.name}</p>
+              </label>
+              <div class='settings'>
+                <i onclick='showMenu(this)' class='uil uil-ellipsis-v'></i>
+                <ul class='task-menu'>
+                  <li><i class='uil uil-trash'></i></li>
+                </ul>
+              </div>
+            </li>`;
+    });
+  }
+  taskBox.innerHTML = li;
+};
+showTodo();
+
+// const taskItem = document.querySelector('.task-item');
+// taskItem.addEventListener('click', updateStatus);
+
+// const showMenu = (selectedTask) => {
+//   console.log(selectedTask);
+// };
+
+// function updateStatus(selectedTask) {
+//   let taskName = selectedTask.parentElement.lastElementChild;
+//   if (selectedTask.checked) {
+//     taskName.classList.add('checked');
+//     todos[selectedTask.id].status = 'completed';
+//   } else {
+//     todos[selectedTask.id].status = 'pending';
+//     taskName.classList.remove('checked');
+//   }
+//   localStorage.setItem('todo-list', JSON.stringify(todos));
+// }
+taskInput.addEventListener('keyup', (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == 'Enter' && userTask) {
+    if (!todos) {
+      todos = [];
+    }
+    let taskInfo = { name: userTask, status: 'pending' };
+    todos.push(taskInfo); // adding new task to todos
+    localStorage.setItem('todo-list', JSON.stringify(todos));
+  }
+  showTodo();
+});
